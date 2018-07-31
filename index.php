35608dae6e910c2afa6a11850fe86e5cf0cb8aaa
rapfjb <41973773+rapfjb@users.noreply.github.com>
2018-07-31 19:47:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿﻿<?php
+$titulo = "Home Page"; // Título desta página
+$css = "css/index.css"; // CSS desta página
+$botao = "home"; // Botão que ficará ativo no menu principal
+require ('_header.php'); // Executa o cabeçalho da página
+?>
+
+<h2>Bem-vindo ao Site of Wolf</h2>
+<p>Blá blá blá blá blá blá blá blá blá blá blá blá blá blá</p>
+
+<?php
+require ('_footer.php');
+?>
